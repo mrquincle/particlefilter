a3b46fdeaa963015b464abe5e16f1411edc042f5
Anne van Rossum <anne@almende.org>
2012-07-26 23:49:01 +0200

    Added CImg.h just for testing

--- test/main.cpp
+++ test/main.cpp
@@ -23,6 +23,10 @@
 
 #include <ParticleFilter.hpp>
 
+#include <CImg.h>
+
+using namespace cimg_library;
+
 /* **************************************************************************************
  * Implementation of main
  * **************************************************************************************/
@@ -42,6 +46,9 @@ public:
 	}
 };
 
+const unsigned char white[3] = {255,255,255}, red[3]    = {120,50,80},
+		yellow[3] = {200,155,0},   green[3]  = {30,200,70};
+
 /**
  * Showcase for a particle filter. It - for that reason - uses images, because it is
  * easiest to tell what is going on, using image processing.
@@ -49,6 +56,26 @@ public:
 int main() {
 	PositionParticleFilter pf;
 
-//	pf.
+	std::string file = "/home/anne/mydata/active_wheel_camera/t1.jpg";
+	CImg<unsigned char> img(file.c_str());
+
+	CImgDisplay main_disp(img, "Click a point");
+
+
+	while (!main_disp.is_closed() && !main_disp.is_keyESC()) {
+		main_disp.wait();
+		if (main_disp.button() && main_disp.mouse_y()>=0) {
+			const int y = main_disp.mouse_y();
+			//			img.draw_rectangle;
+			//			img.draw_rectangle(0,y0,img.width()-1,y0+12,red);
+			// from http://www.codeproject.com/answers/55965/How-to-select-an-area-from-displayed-image-using-C.aspx#answer2
+			CImg <int> SelectedImageCords = img.get_select(main_disp,2,0);
+			CImg <unsigned char> SelectedImage = img.get_crop(SelectedImageCords(0),SelectedImageCords(1),SelectedImageCords(3),SelectedImageCords(4));
+			CImgDisplay disp2(SelectedImage,"Selected Image");
+			while(!disp2.is_closed() && !disp2.is_keyESC()) {
+				disp2.wait();
+			}
+		}
+	}
 }
 
